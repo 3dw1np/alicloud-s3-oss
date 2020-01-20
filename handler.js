affc69aae44016f43bab7e0d401df11abd146db0
Edwin POLYCARPE <edwin.polycarpe@gmail.com>
2020-01-20 13:13:27 +0100

    Draft replicate function (orignal code from https://github.com/serverless/examples/blob/master/aws-node-s3-file-replicator/handler.js)

--- handler.js
+++ handler.js
@@ -4,7 +4,38 @@ const AWS = require('aws-sdk');
 
 const s3 = new AWS.S3();
 
-module.exports.hello = async event => {
-  console.log('Event:', event);
-  return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const s3BucketName = process.env.S3_BUCKET_NAME
+
+module.exports.replicate = async event => {
+  if (!s3BucketName) {
+    context.fail('Error: Environment variable S3_BUCKET_NAME missing')
+    return
+  }
+  if (event.Records === null) {
+    context.fail('Error: Event has no records.')
+    return
+  }
+
+  let tasks = []
+  for (let i = 0; i < event.Records.length; i++) {
+    tasks.push(replicatePromise(event.Records[i]))
+  }
+
+  Promise.all(tasks)
+    .then(() => { context.succeed() })
+    .catch(() => { context.fail() })
 };
+
+function replicatePromise(record) {
+  return new Promise((resolve, reject) => {
+    const srcBucket = record.s3.bucket.name
+    const srcKey = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "))
+
+    const destBucket = 'OSS';
+    const destKey = 'null';
+
+    var msg = 'Copying from S3 ' + srcBucket + ':' + srcKey + ' to OOS ' + destBucket + ':' + destKey;
+
+    console.log('Attempting: ' + msg)
+  })
+}
